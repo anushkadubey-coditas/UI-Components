a9342728f47a402ddd32ed120ea9a167b441969e
Anushka Dubey <anushka.dubey@coditas.com>
2025-05-12 17:53:49 +0530

    Validation Updated

--- UI-Components/projects/ui-components/src/lib/components/input/ui-textarea/ui-textarea.component.spec.ts
+++ UI-Components/projects/ui-components/src/lib/components/input/ui-textarea/ui-textarea.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UiTextareaComponent } from './ui-textarea.component';
+
 
 describe('UiTextareaComponent', () => {
   let component: UiTextareaComponent;
